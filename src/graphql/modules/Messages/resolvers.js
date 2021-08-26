bbada045a378f0ef980da8693d0a421f59f1f719
samuel-neves <samuel212584@gmail.com>
2021-08-26 11:37:51 -0300

    fix: query bug correction

--- src/graphql/modules/Messages/resolvers.js
+++ src/graphql/modules/Messages/resolvers.js
@@ -2,8 +2,8 @@ import Message from "../../../database/models/Message";
 
 export default {
   Query: {
-    messages: () => Message.find(),
-    messagesByRoom: (_, { room }) => Message.find({ room: room }),
+    messages: async () => await Message.find().sort({ date:-1 }),
+    messagesByRoom: async (_, { room }) => await Message.find({ room: room }).sort({ date:-1 }),
   },
   Mutation: {
     createMessage: (_, { data }) => Message.create(data),
